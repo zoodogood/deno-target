ac807694b14688f46a7490a5467648521364242c
zoodogood <zoodogood@gmail.com>
2022-10-27 21:08:59 +0300

    Enviroment name change

--- src/core/mod.ts
+++ src/core/mod.ts
@@ -5,7 +5,7 @@
 import 'https://deno.land/x/dotenv@v3.2.0/load.ts';
 import { createClient, Client } from "#core/Client/mod.ts";
 
-const token: string = Deno.env.get("TOKEN")!;
+const token: string = Deno.env.get("DISCORD_TOKEN")!;
 const client: Client = createClient(token);
 
 
